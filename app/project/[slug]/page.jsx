4d35cd1fd6e78e0e180e6a34f939727f7dbcd355
Gonçalo Andrade <goncaloandradedovale@gmail.com>
2024-07-27 22:39:46 +0000

    related projects

--- app/project/[slug]/page.jsx
+++ app/project/[slug]/page.jsx
@@ -1,15 +1,25 @@
 "use server";
+import style from "./page.module.scss";
 import portableTextImage from "../../utils/portableText/portableTextImage";
 import sanityFetch from "../../utils/sanityFetch";
 import { PortableText } from "@portabletext/react";
+import TileGrid from "../../../component/TileGrid/TileGrid";
 
-const Project = ({ params }) => {
-  return sanityFetch({ query: `*[_type == 'project' && slug.current == '${params.slug}']` }).then((data) => {
-    return <PortableText
-      	value={data[0].Body}
-      	components={portableTextImage}
-    	/>
-  })
+const Project = async ({ params }) => {
+  const data = await sanityFetch({ query: `*[_type == 'project' && slug.current == '${params.slug}'] {
+  ...,
+  tools[]->,
+  relatedProjects[]->
+}` });
+
+  return <div className={style.projectContainer}>
+    <PortableText
+      value={data[0].Body}
+      components={portableTextImage}
+    />
+    <h2>You might also like</h2>
+    <TileGrid data={data}/>
+    </div>
 };
 
 export default Project;
