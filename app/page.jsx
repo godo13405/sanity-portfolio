a1a19090c3cf78cbe07715a6e932b9c6ca809fde
Gonçalo Andrade <goncaloandradedovale@gmail.com>
2024-07-27 22:54:55 +0000

    fonts and hero section

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,16 @@
 "use server";
 import TileGrid from "../component/TileGrid/TileGrid";
 import sanityFetch from "./utils/sanityFetch";
+import "../styles/global.scss";
+import Hero from "../component/Hero/Hero";
 
 const Home = () => {
-  return sanityFetch({ query: `*[_type == 'project']` }).then((data) => <TileGrid data={data} />);
+  return <main>
+    <Hero />
+    <div class="contained">
+      {sanityFetch({ query: `*[_type == 'project']` }).then((data) => <TileGrid data={data} />)}
+    </div>
+    </main>
 };
 
 export default Home;
---
+++ component/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import style from "./style.module.scss"
+
+const Hero = () => {
+    return <div className={style.heroContainer}>
+        <div class="contained">
+            <h1 >Goncalo Andrade is a Digital Designer in London</h1>
+        </div>
+    </div>
+}
+
+
+export default Hero;
