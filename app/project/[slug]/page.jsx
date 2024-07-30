148ff9e5df3a0f94a7eb49d6b05a457e90e2d3af
Gonçalo Andrade <goncaloandradedovale@gmail.com>
2024-07-30 09:55:13 +0000

    better project hero styling, font fix hopefully

--- app/project/[slug]/page.jsx
+++ app/project/[slug]/page.jsx
@@ -19,7 +19,9 @@ const Project = async ({ params }) => {
 const data = dataArr[0];
 
   return <article className={style.projectContainer}>
-    <div className={style.hero} style={{backgroundImage:`url(${data.imageUrl})`,backgroundColor: `${data.color ? data.color.hex : 'grey'}`}}></div>
+    <div className={style.hero} style={{backgroundColor: `${data.color ? data.color.hex : 'grey'}`}}>
+    <img className={`${style.inner} contained`} src={data.imageUrl} />
+    </div>
     <div className={`${style.projectContainerInner} contained`}>
       <h1>{data.name}</h1>
       <PortableText
