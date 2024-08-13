61e22fc3471107bd1918626b29284c6afd7b3da0
Gonçalo Andrade <goncaloandradedovale@gmail.com>
2024-08-13 10:27:37 +0000

    experience timeline fade-in

--- component/ExperienceTile/ExperienceTile.jsx
+++ component/ExperienceTile/ExperienceTile.jsx
@@ -1,14 +1,21 @@
+"use client";
+
+import { InView } from "react-intersection-observer";
 import dateTransform from "../../app/utils/dateTransform";
 import style from "./style.module.scss";
 
-const ExperienceTile = ({data}) => {
-    return <div className={`${style.tileContainer}`}>
-        <h3>{data.name}</h3>
-        <div className={style.secondaryContainer}>
-            <img src={data.company.imageUrl} title={data.company.name} />
-            {dateTransform(data.startDate)} - {dateTransform(data.endDate)}
-        </div>
-    </div>
+const ExperienceTile = ({ data, k }) => {
+    return <InView key={k}>
+        {({ inView, ref, entry }) => {
+            console.log(entry);
+            return <div className={`${style.tileContainer} ${entry && entry.isIntersecting ? style.inView :''}`} ref={ref}>
+            <h3>{data.name}</h3>
+            <div className={style.secondaryContainer}>
+                <img src={data.company.imageUrl} title={data.company.name} />
+                {dateTransform(data.startDate)} - ${dateTransform(data.endDate)}
+            </div>
+        </div>}}
+    </InView>
 }
 
 export default ExperienceTile;
