ea7f2918cae60ebeeaa8a177423b36e86e0a1554
Goncalo Stratford Andrade <goncalo.andrade@global.com>
2024-08-27 12:52:32 +0100

    GA fixed

--- app/layout.js
+++ app/layout.js
@@ -1,35 +1,40 @@
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import localFont from 'next/font/local'
-import Nav from "../component/Nav/Nav"
-import Footer from "../component/Footer/Footer"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import localFont from "next/font/local";
+import Nav from "../component/Nav/Nav";
+import Footer from "../component/Footer/Footer";
 import { VercelToolbar } from "@vercel/toolbar/next";
-import GoogleAnalytics from "./utils/GoogleAnalytics";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import colours from "../styles/_theme.module.scss";
 
 const GTUltra = localFont({
   src: [
-    { path: '../public/font/GT-Ultra-Median-Black-Trial.woff2', 
-      weight: '900',
-      style: 'normal'
+    {
+      path: "../public/font/GT-Ultra-Median-Black-Trial.woff2",
+      weight: "900",
+      style: "normal",
     },
-    { path: '../public/font/GT-Ultra-Median-Bold-Trial.woff2', 
-      weight: '700',
-      style: 'normal'
+    {
+      path: "../public/font/GT-Ultra-Median-Bold-Trial.woff2",
+      weight: "700",
+      style: "normal",
     },
-    { path: '../public/font/GT-Ultra-Standard-Bold-Trial.woff2', 
-      weight: '400',
-      style: 'normal'
+    {
+      path: "../public/font/GT-Ultra-Standard-Bold-Trial.woff2",
+      weight: "400",
+      style: "normal",
     },
-    { path: '../public/font/GT-Ultra-Standard-Regular-Trial.woff2', 
-      weight: '400',
-      style: 'bold'
+    {
+      path: "../public/font/GT-Ultra-Standard-Regular-Trial.woff2",
+      weight: "400",
+      style: "bold",
     },
-    { path: '../public/font/GT-Ultra-Standard-Regular-Italic-Trial.woff2', 
-      weight: '400',
-      style: 'italic'
+    {
+      path: "../public/font/GT-Ultra-Standard-Regular-Italic-Trial.woff2",
+      weight: "400",
+      style: "italic",
     },
   ],
-})
+});
 
 export const metadata = {
   metadataBase: new URL("https://goncalo.stratfordandrade.com"),
@@ -44,7 +49,6 @@ export default function RootLayout({ children }) {
   const shouldInjectToolbar = process.env.NODE_ENV === "development";
   return (
     <html lang="en" className={GTUltra.className}>
-        <GoogleAnalytics />
       <body>
         <Nav />
         <div>{children}</div>
@@ -52,6 +56,7 @@ export default function RootLayout({ children }) {
         <SpeedInsights />
         {shouldInjectToolbar && <VercelToolbar />}
       </body>
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID} />
     </html>
   );
 }
