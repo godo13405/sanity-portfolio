b61ff774e3638523aca16eb4f9b86db37daea39e
Goncalo Stratford Andrade <goncalo.andrade@global.com>
2024-08-30 12:17:22 +0100

    fix: CV doesnt open in a new tab

--- component/IconButton/IconButton.jsx
+++ component/IconButton/IconButton.jsx
@@ -1,9 +1,21 @@
-import style from "./style.module.scss"
+import style from "./style.module.scss";
 
-const IconButton = ({className = '', href = '/', newTab = false, img = 'face.svg'}) => {
-return <a className={`${className} ${style.iconButton} ${style.iconButtonCta}`} href={href} target="_blank" style={{
-    backgroundImage: `url(/img/${img})`
-}} />
-}
+const IconButton = ({
+  className = "",
+  href = "/",
+  newTab = false,
+  img = "face.svg",
+}) => {
+  return (
+    <a
+      className={`${className} ${style.iconButton} ${style.iconButtonCta}`}
+      href={href}
+      target={newTab ? "_blank" : ""}
+      style={{
+        backgroundImage: `url(/img/${img})`,
+      }}
+    />
+  );
+};
 
 export default IconButton;
