93052c4eca4f28037cb555b2b801db03223d1392
Goncalo Stratford Andrade <goncaloandradedovale@gmail.com>
2025-01-13 11:13:20 +0000

    image 404 fallback

--- app/utils/portableText/portableTextImage.js
+++ app/utils/portableText/portableTextImage.js
@@ -1,19 +1,29 @@
-import style from "./style.module.scss"
+import style from "./style.module.scss";
 
 const portableTextImage = {
   types: {
     image: ({ value, isInline }) => {
-      return (
-        <figure className={style.figure}>
-          <img
-          className={`${value.asset.hasTransparency ? style.transparent : ''} ${value.asset.tags && value.asset.tags.padding ? style.padding : ''}`}
-          src={`${value.asset.url}?w=800&fit=max&auto=format`}
-          alt={value.asset.altText}
-        />
-        {value.asset.title && <caption>{value.asset.title}</caption>}
-        </figure>
-      );
-      },
+      if (value && value.asset) {
+        return (
+          <figure className={style.figure}>
+            <img
+              className={`${
+                value.asset.hasTransparency ? style.transparent : ""
+              } ${
+                value.asset.tags && value.asset.tags.padding
+                  ? style.padding
+                  : ""
+              }`}
+              src={`${value.asset.url}?w=800&fit=max&auto=format`}
+              alt={value.asset.altText}
+            />
+            {value.asset.title && <caption>{value.asset.title}</caption>}
+          </figure>
+        );
+      } else {
+        console.log("failed to load image", value);
+      }
+    },
   },
 };
 
