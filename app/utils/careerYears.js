d5a851226f68da946cd53de1705c35a45e046a6c
Gonçalo Andrade <goncaloandradedovale@gmail.com>
2025-04-14 10:12:31 +0000

    changed date calculation to jan 1

--- app/utils/careerYears.js
+++ app/utils/careerYears.js
@@ -1,5 +1,5 @@
 const CareerYears = () => {
-  var prevTime = new Date('1 July 2010'); // career start
+  var prevTime = new Date('1 January 2010'); // career start
   var thisTime = new Date(); // now
   var diff = Math.floor((thisTime.getTime() - prevTime.getTime()) / (1000 * 60 * 60 * 24 * 364)); // now - career start
 
